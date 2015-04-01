15639c448a748aa2e1b6ac02f2f054094028988f
Mihai Nicolae <m.nicolae91@gmail.com>
2015-04-01 12:09:53 -0400

    pushing tests to debug sum_out_variable, which seems to not be working as expected.

--- bnetbase.py
+++ bnetbase.py
@@ -531,3 +531,73 @@ def VE(Net, QueryVar, EvidenceVars):
     # normalize the distribution
     normalizedDistr = [i/max(multipliedFactor.values) for i in multipliedFactor.values]
     return normalizedDistr
+
+if __name__ == '__main__':
+    VisitAsia = Variable('Visit_To_Asia', ['visit', 'no-visit'])
+    F1 = Factor("F1", [VisitAsia])
+    F1.add_values([['visit', 0.01], ['no-visit', 0.99]])
+    
+    Smoking = Variable('Smoking', ['smoker', 'non-smoker'])
+    F2 = Factor("F2", [Smoking])
+    F2.add_values([['smoker', 0.5], ['non-smoker', 0.5]])
+    
+    Tuberculosis = Variable('Tuberculosis', ['present', 'absent'])
+    F3 = Factor("F3", [Tuberculosis, VisitAsia])
+    F3.add_values([['present', 'visit', 0.05],
+                   ['present', 'no-visit', 0.01],
+                   ['absent', 'visit', 0.95],
+                   ['absent', 'no-visit', 0.99]])
+    
+    Cancer = Variable('Lung Cancer', ['present', 'absent'])
+    F4 = Factor("F4", [Cancer, Smoking])
+    F4.add_values([['present', 'smoker', 0.10],
+                   ['present', 'non-smoker', 0.01],
+                   ['absent', 'smoker', 0.90],
+                   ['absent', 'non-smoker', 0.99]])
+    
+    Bronchitis = Variable('Bronchitis', ['present', 'absent'])
+    F5 = Factor("F5", [Bronchitis, Smoking])
+    F5.add_values([['present', 'smoker', 0.60],
+                   ['present', 'non-smoker', 0.30],
+                   ['absent', 'smoker', 0.40],
+                   ['absent', 'non-smoker', 0.70]])
+    
+    TBorCA = Variable('Tuberculosis or Lung Cancer', ['true', 'false'])
+    F6 = Factor("F6", [TBorCA, Tuberculosis, Cancer])
+    F6.add_values([['true', 'present', 'present', 1.0],
+                   ['true', 'present', 'absent', 1.0],
+                   ['true', 'absent', 'present', 1.0],
+                   ['true', 'absent', 'absent', 0],
+                   ['false', 'present', 'present', 0],
+                   ['false', 'present', 'absent', 0],
+                   ['false', 'absent', 'present', 0],
+                   ['false', 'absent', 'absent', 1]])
+    
+    
+    Dyspnea = Variable('Dyspnea', ['present', 'absent'])
+    F7 = Factor("F7", [Dyspnea, TBorCA, Bronchitis])
+    F7.add_values([['present', 'true', 'present', 0.9],
+                   ['present', 'true', 'absent', 0.7],
+                   ['present', 'false', 'present', 0.8],
+                   ['present', 'false', 'absent', 0.1],
+                   ['absent', 'true', 'present', 0.1],
+                   ['absent', 'true', 'absent', 0.3],
+                   ['absent', 'false', 'present', 0.2],
+                   ['absent', 'false', 'absent', 0.9]])
+    
+    
+    Xray = Variable('XRay Result', ['abnormal', 'normal'])
+    F8 = Factor("F8", [Xray, TBorCA])
+    F8.add_values([['abnormal', 'true', 0.98],
+                   ['abnormal', 'false', 0.05],
+                   ['normal', 'true', 0.02],
+                   ['normal', 'false', 0.95]])
+    
+    Asia = BN("Asia", [VisitAsia, Smoking, Tuberculosis, Cancer,
+                       Bronchitis, TBorCA, Dyspnea, Xray],
+                       [F1, F2, F3, F4, F5, F6, F7, F8])
+    
+    F6.print_table()
+    n1 = sum_out_variable(F6, TBorCA)
+    n1.print_table()
+    
